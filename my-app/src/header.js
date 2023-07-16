8acae1ae095a2b71041ee6d199d67f7c2157e02f
Edinorig <super-ddred@ukr.net>
2023-07-17 00:49:50 +0200

    First commit

---
+++ my-app/src/header.js
@@ -0,0 +1,13 @@
+import  './header.css'
+
+
+export  function Header() {
+    return (
+        <header>
+            <h1>ReactFacts</h1>
+            <h4>React cource - project 1</h4>
+        </header>
+    );
+}
+
+export default Header
